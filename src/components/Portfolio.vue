27e04cef5e179a7dfc448136fe93084f3144e196
Torrence Brown <torrenceb90@gmail.com>
2021-12-23 10:58:37 -0700

    Removed portfolio object from carousel

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container id="portfolio" class="mr-8 mb-4" style="min-height: 40vh" fluid>
+  <v-container id="portfolio" class="mr-8 mb-4" fluid>
     <h1 class="text-left mb-2">{{ "The Works".toUpperCase() }}</h1>
     <v-row>
       <v-col>
@@ -7,6 +7,7 @@
           <v-carousel hide-delimiters height="setCarouselHeight">
             <v-carousel-item v-for="project in projects" :key="project.id">
               <v-img
+                :alt="project.alt"
                 :src="project.image"
                 :min-height="setCarouselHeight"
               ></v-img>
@@ -48,19 +49,13 @@ export default {
     projects: [
       {
         id: Math.floor(Math.random() * 1000),
-        title: "Portfolio Page",
-        text:
-          "This portfolio page was carefully crafted and designed with minimalism and functionality in mind. Although simple, I chose to use Vue.js as my Frontend frameword to build this page because I'm fascinated with the vast ecosystem that it offers, allowing developers to design robust Frontend applications.",
-        image: require("@/assets/torrenceb_icon.png"),
-      },
-      {
-        id: Math.floor(Math.random() * 1000),
         title: "Timekeeping Feature",
         text:
           "As the main focus for a summer internship, I was tasked with the responsibility of developing a mock up for what would eventually become the interface for an employee timekeeping feature of a company’s proprietary Content Management System. The mock ups were built exclusively with HTML, CSS and JavaScript and featured religious use of semantic HTML and CSS Flexbox. The stylesheet in particular was designed in a way that CSS classes were almost exclusively comprised of utility classes, rather than being content specific. This allowed for complete independence of the stylesheet, encouraging re-use and object composition. This feature went on to solve a long standing issue that the company had, effectively creating a single source of truth where contracted employees were able to enter hours worked by time period and these entries would allow for an administrator to review and approve hours accordingly.\
         Stack: HTML, CSS, JavaScript",
         link: "",
         image: require("@/assets/ss-1.png"),
+        alt: "Timecard",
       },
       {
         id: Math.floor(Math.random() * 1000),
@@ -69,6 +64,7 @@ export default {
           "The first piece of a proprietary Hotel Operations Management System for an Upwork client, this project was accomplished alongside two developer friends in the scope of 30 days. Acting as the primary liaison between the client and our agency, a healthy rapport was established between myself and the client. This resulted in a polished and functional product which solved the client’s problem of needing a way to assign tickets to hotel staff. This application included ways to prioritize duties to specific staff members, to categorize and filter tickets depending on what Hotel department (e.g Housekeeping, Front Desk, etc.) and assign each ticket to a location of the hotel which service was required. In addition, this application gave the client the ability to dynamically add staff members and hotel rooms via an administrative view. All data entered, deleted and updated by client was persisted through a Firebase backend.\
         Stack: Vue.js, Vue Router, Vuex, Vuetify, Firebase.",
         image: require("@/assets/som_sample.png"),
+        alt: "Oms",
       },
       {
         id: Math.floor(Math.random() * 1000),
@@ -78,6 +74,7 @@ export default {
         Stack: React.js, Firebase",
         link: "https://netflix-clone-ceb9d.web.app/",
         image: require("@/assets/netflix_sample.png"),
+        alt: "Netflix Clone",
       },
       {
         id: Math.floor(Math.random() * 1000),
@@ -85,6 +82,7 @@ export default {
         text:
           "I define this project as the first true application of my development skills to a real world project. My key responsibility was to design an interface for an application of which holistic Health Practitioners were able to maintain accountable relationships with their clients and manage client records. Stack: Flutter ",
         image: require("@/assets/amch_sample.png"),
+        alt: "Practitioner App",
       },
     ],
   }),
